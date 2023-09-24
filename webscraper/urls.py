5c0e6e32cb99738f20b43377a41dde4bbe0cddcd
Esmond Adjei <xmongram@gmail.com>
2023-09-24 15:54:18 +0000

    modified:   webscraper/urls.py

--- webscraper/urls.py
+++ webscraper/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from base import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(urls)),
+    path('', include('base.urls')),
 ]
